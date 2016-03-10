c5619c2da00a00fdbb1c83a17617057d6a0ca1a2
Oliver Feindt <ofeindt@hotmail.com>
2016-03-10 17:54:26 +0100

    templates for base, plot app and registration changed. Separate css and js for each.

---
+++ codea/stockplot/static/stockplot/js/basescript.js
@@ -0,0 +1,39 @@
+//----------------------------------------------------------------------
+//-----------Javascript base--------------------------------------------
+//----------------------------------------------------------------------
+
+// main document ready function ----------------------------------------
+$(document).ready(function(){
+	
+    // collapsed navbar
+	$('#nav-button').on('click touchstart', function () {
+		
+		if($('.navbar-collapse').hasClass('in')){
+			$('#span-top').css('animation', 'rotate2 0.2s ease-in forwards');
+			$('#span-bottom').css('animation', 'rotateBack2 0.2s ease-in forwards');
+			$('#span-top').removeClass('span-top-rotated');
+			$('#span-bottom').removeClass('span-bottom-rotated');
+		} else {
+			$('#span-top').addClass('span-top-rotated');
+			$('#span-bottom').addClass('span-bottom-rotated');
+			$('#span-top').css('animation', 'rotate 0.2s ease-in forwards');
+			$('#span-bottom').css('animation', 'rotateBack 0.2s ease-in forwards');
+		}
+	});
+	
+	// animation for smooth scroll:
+	$('a[href*=#]').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top - 50;
+                $('html,body').animate({scrollTop: targetOffset}, 1000);
+                return false;
+            }
+        }
+    });
+
+});
+// end of document ready funtion!---------------------------------------
